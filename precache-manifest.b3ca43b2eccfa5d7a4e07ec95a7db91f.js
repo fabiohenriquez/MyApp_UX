f0ec47c94cd8b162399dac253ec743c3b80ea877
Fabio <fa_henriquez@hotmail.com>
2019-08-18 16:49:38 -0600

    Updates

---
+++ precache-manifest.b3ca43b2eccfa5d7a4e07ec95a7db91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea270ca4d52774cdf69659907735361",
+    "url": "/MyApp_UX/index.html"
+  },
+  {
+    "revision": "c11efacf4d02e82679ab",
+    "url": "/MyApp_UX/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "31fefca5c6291a72dbd6",
+    "url": "/MyApp_UX/static/js/2.ae086021.chunk.js"
+  },
+  {
+    "revision": "c11efacf4d02e82679ab",
+    "url": "/MyApp_UX/static/js/main.e569a926.chunk.js"
+  },
+  {
+    "revision": "937c3f808c276f7d3a48",
+    "url": "/MyApp_UX/static/js/runtime~main.91010f66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyApp_UX/static/media/logo.5d5d9eef.svg"
+  }
+]);
